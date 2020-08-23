3b95b4b5c018355cdee98f3ca6fefb08e65cec67
Ernest Guevarra <ernest@guevarra.io>
2020-08-24 00:47:46 +0100

    create datasets

--- R/data.R
+++ R/data.R
@@ -1,1 +1,167 @@
+################################################################################
+#
+#'
+#' Mozambique country borders
+#'
+#' @format A SpatialPolygonsDataFrame object containing country borders of
+#'   Mozambique
+#'
+#' | **Variables** | **Description** |
+#' | :--- | :--- |
+#' | Shape_Leng | Shape length |
+#' | Shape_Area | Shape area |
+#' | ADM0_EN | Administrative area level 0 name in English |
+#' | ADM0_PT | Administrative area level 0 name in Portuguese |
+#' | ADM0_PCODE | Administrative area level 0 postal code |
+#' | ADM0_REF | Administrative area level 0 reference |
+#' | ADM0ALT1EN | Administrative area level 0 alternative English name 1 |
+#' | ADM0ALT2EN | Administrative area level 0 alternative English name 2 |
+#' | ADM0ALT1PT | Administrative area level 0 alternative Portuguese name 1 |
+#' | ADM0ALT2PT | Administrative area level 0 alternative Portuguese name 2 |
+#' | date | Date created |
+#' | validOn | Data valid on |
+#' | validTo | Data valid to |
+#'
+#' @examples
+#' country
+#'
+#' @source https://data.humdata.org/dataset/mozambique-administrative-levels-0-3
+#'
+#'
+#
+################################################################################
+"country"
 
+
+################################################################################
+#
+#'
+#' Mozambique provincias borders
+#'
+#' @format A SpatialPolygonsDataFrame object containing provincias borders of
+#'   Mozambique
+#'
+#' | **Variables** | **Description** |
+#' | :--- | :--- |
+#' | Shape_Leng | Shape length |
+#' | Shape_Area | Shape area |
+#' | ADM1_PT | Administrative area level 1 name in Portuguese |
+#' | ADM1_PCODE | Administrative area level 1 postal code |
+#' | ADM1_REF | Administrative area level 1 reference |
+#' | ADM1ALT1PT | Administrative area level 1 alternative Portuguese name 1 |
+#' | ADM1ALT2PT | Administrative area level 1 alternative Portuguese name 2 |
+#' | ADM0_EN | Administrative area level 0 name in English |
+#' | ADM0_PT | Administrative area level 0 name in Portuguese |
+#' | ADM0_PCODE | Administrative area level 0 postal code |
+#' | date | Date created |
+#' | validOn | Data valid on |
+#' | validTo | Data valid to |
+#'
+#' @examples
+#' provincias
+#'
+#' @source https://data.humdata.org/dataset/mozambique-administrative-levels-0-3
+#'
+#
+################################################################################
+"provincias"
+
+
+################################################################################
+#
+#'
+#' Mozambique distritos borders
+#'
+#' @format A SpatialPolygonsDataFrame object containing distritos borders of
+#'   Mozambique
+#'
+#' | **Variables** | **Description** |
+#' | :--- | :--- |
+#' | Shape_Leng | Shape length |
+#' | Shape_Area | Shape area |
+#' | ADM2_PT | Administrative area level 2 name in Portuguese |
+#' | ADM2_PCODE | Administrative area level 2 postal code |
+#' | ADM2_REF | Administrative area level 2 reference |
+#' | ADM2ALT1PT | Administrative area level 2 alternative Portuguese name 1 |
+#' | ADM2ALT2PT | Administrative area level 2 alternative Portuguese name 2 |
+#' | ADM1_PT | Administrative area level 1 name in Portuguese |
+#' | ADM1_PCODE | Administrative area level 1 postal code |
+#' | ADM0_EN | Administrative area level 0 name in English |
+#' | ADM0_PT | Administrative area level 0 name in Portuguese |
+#' | ADM0_PCODE | Administrative area level 0 postal code |
+#' | date | Date created |
+#' | validOn | Data valid on |
+#' | validTo | Data valid to |
+#'
+#' @examples
+#' distritos
+#'
+#' @source https://data.humdata.org/dataset/mozambique-administrative-levels-0-3
+#'
+#
+################################################################################
+"distritos"
+
+
+################################################################################
+#
+#'
+#' Mozambique postos borders
+#'
+#' @format A SpatialPolygonsDataFrame object containing postos borders of
+#'   Mozambique
+#'
+#' | **Variables** | **Description** |
+#' | :--- | :--- |
+#' | Shape_Leng | Shape length |
+#' | Shape_Area | Shape area |
+#' | ADM3_PT | Administrative area level 3 name in Portuguese |
+#' | ADM3_PCODE | Administrative area level 3 postal code |
+#' | ADM3_REF | Administrative area level 3 reference |
+#' | ADM3ALT1PT | Administrative area level 3 alternative Portuguese name 1 |
+#' | ADM3ALT2PT | Administrative area level 3 alternative Portuguese name 2 |
+#' | ADM2_PT | Administrative area level 2 name in Portuguese |
+#' | ADM2_PCODE | Administrative area level 2 postal code |
+#' | ADM1_PT | Administrative area level 1 name in Portuguese |
+#' | ADM1_PCODE | Administrative area level 1 postal code |
+#' | ADM0_EN | Administrative area level 0 name in English |
+#' | ADM0_PT | Administrative area level 0 name in Portuguese |
+#' | ADM0_PCODE | Administrative area level 0 postal code |
+#' | date | Date created |
+#' | validOn | Data valid on |
+#' | validTo | Data valid to |
+#'
+#' @examples
+#' postos
+#'
+#' @source https://data.humdata.org/dataset/mozambique-administrative-levels-0-3
+#'
+#
+################################################################################
+"postos"
+
+
+################################################################################
+#
+#'
+#' Mozambique villages locations
+#'
+#' @format A SpatialPointsDataFrame object containing villages locations of
+#'   Mozambique
+#'
+#' | **Variables** | **Description** |
+#' | osm_id | OpenStreetMap Identifier |
+#' | name | Name of location |
+#' | source | Source of location data |
+#' | population | Population size |
+#' | is_in | Is in what country location |
+#' | place | Type of location |
+#'
+#' @examples
+#' villages
+#'
+#' @source https://data.humdata.org/dataset/hotosm_moz_populated_places
+#'
+#
+################################################################################
+"villages"
